69559c281aff72ceb001b53ed00f4e301d6cfd3d
raven <740585947@qq.com>
2023-02-13 18:23:18 +0800

    Initial commit

---
+++ src/main/java/top/mccat/StrengthPlus.java
@@ -0,0 +1,42 @@
+package top.mccat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Raven
+ * @date 2022/05/05 16:05
+ * 主插件启动类
+ */
+public class StrengthPlus extends JavaPlugin {
+    /**
+     * 被spigot读取的方法
+     */
+    @Override
+    public void onLoad() {
+
+    }
+
+    /**
+     * 插件被允许时的方法
+     */
+    @Override
+    public void onEnable() {
+    }
+
+    /**
+     * 禁用时的方法
+     */
+    @Override
+    public void onDisable() {
+
+    }
+
+    /**
+     * 重载配置方法
+     */
+    @Override
+    public void reloadConfig() {
+
+    }
+
+}
